1d2bb32805b68c233d13cf8535c9e73cdb2cb1fb
Gabriel <gabriel.dearden@frends.com>
2025-07-18 10:19:04 +0300

    Improved HeaderParser

--- Frends.Shopify.GetOrders/Frends.Shopify.GetOrders/Helpers/HeaderParser.cs
+++ Frends.Shopify.GetOrders/Frends.Shopify.GetOrders/Helpers/HeaderParser.cs
@@ -30,22 +30,40 @@ namespace Frends.Shopify.GetOrders.Helpers
 
                     if (item.Contains("rel=\"next\""))
                     {
-                        var start = item.IndexOf("page_info=", StringComparison.Ordinal) + "page_info=".Length;
-                        var end = item.IndexOfAny(['>', '&'], start);
-                        if (start > 0 && end > start)
-                            pageInfo.NextPage = item[start..end];
+                        var cursor = ExtractPageInfoCursor(item);
+                        if (!string.IsNullOrEmpty(cursor))
+                            pageInfo.NextPage = cursor;
                     }
                     else if (item.Contains("rel=\"previous\""))
                     {
-                        var start = item.IndexOf("page_info=", StringComparison.Ordinal) + "page_info=".Length;
-                        var end = item.IndexOfAny(['>', '&'], start);
-                        if (start > 0 && end > start)
-                            pageInfo.PreviousPage = item[start..end];
+                        var cursor = ExtractPageInfoCursor(item);
+                        if (!string.IsNullOrEmpty(cursor))
+                            pageInfo.PreviousPage = cursor;
                     }
                 }
             }
 
             return pageInfo;
+        }
+
+        /// <summary>
+        /// Extracts the page_info cursor value from a link header item
+        /// </summary>
+        private static string ExtractPageInfoCursor(string linkItem)
+        {
+            var pageInfoIndex = linkItem.IndexOf("page_info=", StringComparison.Ordinal);
+            if (pageInfoIndex == -1) return string.Empty;
+
+            var start = pageInfoIndex + "page_info=".Length;
+            if (start >= linkItem.Length) return string.Empty;
+
+            var end = linkItem.IndexOfAny(['>', '&'], start);
+            if (end == -1) end = linkItem.Length;
+
+            if (end <= start) return string.Empty;
+
+            var cursor = linkItem[start..end];
+            return Uri.UnescapeDataString(cursor);
         }
     }
 }
